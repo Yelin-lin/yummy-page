f42f60daa7f23113cf871dd58faa18e114f8d876
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-06-08 13:15:12 +0900

    Update: venv 삭제 및 재설치

--- subpy.py
+++ subpy.py
@@ -20,14 +20,16 @@ def movie_post():
     hashtags_receive = request.form['hashtags_give']
 
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive,headers=headers)
+    data = requests.get(url_receive, headers=headers)
     
     soup = BeautifulSoup(data.text, 'html.parser')
 
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
     oglocation = soup.select_one('meta[property="og:description"]')['content']
-    ogimage = soup.select_one('span.bg_present') #이거 여쭤보기!!!
+    ogimage = soup.select_one('#mArticle > div.cont_essential > div:nth-child(1) > div.details_present > a > span.bg_present') #이거 여쭤보기!!! ['style'][22:-2]
     ogurl = soup.select_one('meta[property="og:url"]')['content']
+
+    print(ogimage)
 
     doc = {
         'title':ogtitle,
