ef2cc2e8060895b0b23a683e88fe73e68aea37b9
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-06-08 17:26:11 +0900

    update

--- subpy.py
+++ subpy.py
@@ -1,36 +1,25 @@
-import certifi
-from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
-
-ca = certifi.where()
-
-client = MongoClient(
-    'mongodb+srv://sparta:test@cluster0.ijt1e7j.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ijt1e7j.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
-
-
+import certifi
+ca = certifi.where()
 import requests
 from bs4 import BeautifulSoup
-
 # 태준 route
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/yelin')
 def subpage():
     return render_template('subpage.html')
-
 @app.route('/taejun')
 def taejun():
-    return render_template('taejun.html')
-
+    return render_template('subpage.html')
 @app.route('/rec')
 def rec_html():
     return render_template('rec.html')
-
 @app.route("/guestbook", methods=["POST"])
 def guestbook_post():
     name_receive = request.form['name_give']
@@ -40,21 +29,14 @@ def guestbook_post():
         'comment': comment_receive
     }
     db.fan.insert_one(doc)
-
     return jsonify({'msg': '저장 완료!'})
-
-
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
     all_comment = list(db.fan.find({}, {'_id': False}))
     return jsonify({'result': all_comment})
-
 # if __name__ == '__main__':
 #     app.run('0.0.0.0', port=5001, debug=True)
-
 #예린 route
-
-
 @app.route("/goodplace", methods=["POST"])
 def movie_post():
     url_receive = request.form['url_give']
@@ -62,16 +44,12 @@ def movie_post():
     star_receive = request.form['star_give']
     hashtags_receive = request.form['hashtags_give']
     imgurl_receive = request.form['imgurl_give']
-
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
-    
     soup = BeautifulSoup(data.text, 'html.parser')
-
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
     oglocation = soup.select_one('meta[property="og:description"]')['content']
     ogurl = soup.select_one('meta[property="og:url"]')['content']
-
     doc = {
         'title':ogtitle,
         'location':oglocation,
@@ -82,26 +60,25 @@ def movie_post():
         'hashtags': hashtags_receive,
     }
     db.yummy.insert_one(doc)
-
     return jsonify({'msg':'저장완료!'})
-
-@app.route("/taejunplace", methods=["POST"])
+@app.route("/goodplace", methods=["GET"])
+def movie_get():
+    all_goodplace = list(db.yummy.find({},{'_id':False}))
+    return jsonify({'result':all_goodplace})
+#태준 route
+@app.route("/taejun", methods=["POST"])
 def movie_post():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
     star_receive = request.form['star_give']
     hashtags_receive = request.form['hashtags_give']
     imgurl_receive = request.form['imgurl_give']
-
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
-    
     soup = BeautifulSoup(data.text, 'html.parser')
-
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
     oglocation = soup.select_one('meta[property="og:description"]')['content']
     ogurl = soup.select_one('meta[property="og:url"]')['content']
-
     doc = {
         'title':ogtitle,
         'location':oglocation,
@@ -111,16 +88,104 @@ def movie_post():
         'star':star_receive,
         'hashtags': hashtags_receive,
     }
-    db.taejun.insert_one(doc)
-
+    db.taejunyummy.insert_one(doc)
     return jsonify({'msg':'저장완료!'})
-
-
-
-@app.route("/goodplace", methods=["GET"])
+@app.route("/taejun", methods=["GET"])
 def movie_get():
-    all_goodplace = list(db.yummy.find({},{'_id':False}))
-    return jsonify({'result':all_goodplace})
-
+    all_taejunyummy = list(db.taejunyummy.find({},{'_id':False}))
+    return jsonify({'result':all_taejunyummy})
+#명빈 route
+@app.route("/myeongbin", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    star_receive = request.form['star_give']
+    hashtags_receive = request.form['hashtags_give']
+    imgurl_receive = request.form['imgurl_give']
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    oglocation = soup.select_one('meta[property="og:description"]')['content']
+    ogurl = soup.select_one('meta[property="og:url"]')['content']
+    doc = {
+        'title':ogtitle,
+        'location':oglocation,
+        'image':imgurl_receive,
+        'url': ogurl,
+        'comment': comment_receive,
+        'star':star_receive,
+        'hashtags': hashtags_receive,
+    }
+    db.myeongbinyummy.insert_one(doc)
+    return jsonify({'msg':'저장완료!'})
+@app.route("/myeongbin", methods=["GET"])
+def movie_get():
+    all_myeongbinyummy = list(db.myeongbinyummy.find({},{'_id':False}))
+    return jsonify({'result':all_myeongbinyummy})
+#명빈 route
+@app.route("/jia", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    star_receive = request.form['star_give']
+    hashtags_receive = request.form['hashtags_give']
+    imgurl_receive = request.form['imgurl_give']
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    oglocation = soup.select_one('meta[property="og:description"]')['content']
+    ogurl = soup.select_one('meta[property="og:url"]')['content']
+    doc = {
+        'title':ogtitle,
+        'location':oglocation,
+        'image':imgurl_receive,
+        'url': ogurl,
+        'comment': comment_receive,
+        'star':star_receive,
+        'hashtags': hashtags_receive,
+    }
+    db.jiayummy.insert_one(doc)
+    return jsonify({'msg':'저장완료!'})
+@app.route("/jia", methods=["GET"])
+def movie_get():
+    all_jia = list(db.jiayummy.find({},{'_id':False}))
+    return jsonify({'result':all_jia})
+#소연 route
+@app.route("/Leesupia", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    star_receive = request.form['star_give']
+    hashtags_receive = request.form['hashtags_give']
+    imgurl_receive = request.form['imgurl_give']
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    oglocation = soup.select_one('meta[property="og:description"]')['content']
+    ogurl = soup.select_one('meta[property="og:url"]')['content']
+    doc = {
+        'title':ogtitle,
+        'location':oglocation,
+        'image':imgurl_receive,
+        'url': ogurl,
+        'comment': comment_receive,
+        'star':star_receive,
+        'hashtags': hashtags_receive,
+    }
+    db.Leesupiayummy.insert_one(doc)
+    return jsonify({'msg':'저장완료!'})
+@app.route("/Leesupia", methods=["GET"])
+def movie_get():
+    all_Leesupia = list(db.Leesupiayummy.find({},{'_id':False}))
+    return jsonify({'result':all_Leesupia})
+#마무리
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+
+
