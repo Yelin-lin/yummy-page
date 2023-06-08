677909d812556e410c27bf3391ef2c3ac6717280
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-06-08 11:30:53 +0900

    Update : hashtags 오타 수정

--- subpy.py
+++ subpy.py
@@ -17,7 +17,7 @@ def movie_post():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
     star_receive = request.form['star_give']
-    hashtags_receive = request.form['hatags_give']
+    hashtags_receive = request.form['hashtags_give']
 
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive,headers=headers)
